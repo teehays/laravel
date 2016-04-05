df1dd1d66e69f5050edc0f882d229eff0e73f822
Tytn Hays <Tytn@Tytn.org.uk>
2016-04-05 10:28:32 +0100

    Initialise

---
+++ app/Http/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ *Add a new task
+ */
+Route::post('/task', function(Request $request ) {    
+   // 
+});
+
+/**
+ * Delete an existing task
+ */
+
+ Route::delete('/task/{id}', function($id){
+    //
+    
+ });
